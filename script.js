c44410921fa001867448c82bcaab64019f85849a
Alex W <amwiederman15@gmail.com>
2022-12-16 17:20:04 -0500

    added 5 day forcast cards

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var currentEl = document.querySelector(".current")
 var key = "5ac5953f4965de6704e28a9ae8fcf1ff"
 var lat
 var lon
-var city = "goshen";
+var city = "Goshen";
 var currentWind;
 var currentTemp;
 var currentHum;
@@ -21,12 +21,7 @@ var day3 = dayjs().add(3, 'day').format('MM/DD/YYYY');
 var day4 = dayjs().add(4, 'day').format('MM/DD/YYYY');
 var day5 = dayjs().add(5, 'day').format('MM/DD/YYYY');
 
-// currentdayEl.textContent = currentDay;
-card5day1El.textContent = day1;
-card5day2El.textContent = day2;
-card5day3El.textContent = day3;
-card5day4El.textContent = day4;
-card5day5El.textContent = day5;
+
 
 
 // Getting weather info
@@ -56,6 +51,7 @@ function getCords() {
         .then(function (data) {
             var lon = data[0].lon
             var lat = data[0].lat
+            
             // console.log(data[0])
 
             var weatherInfoUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + key
@@ -64,13 +60,88 @@ function getCords() {
                     return response.json();
                 })
                 .then(function (data) {
-                    console.log(data.list)
-                    currentWind = data.list[0].wind.speed
+                    console.log(data.list[0])
+                    currentWind = data.list[0]
 
+                    // Current Day Card
                     var currTitleEl = document.createElement('h4');
-                    currTitleEl.innerHTML = currentDay
+                    currTitleEl.innerHTML = city + " " + currentDay
                     currTitleEl.classList = "card-header"
                     currentEl.appendChild(currTitleEl)
+
+                    var currTempEl = document.createElement("div");
+                    currTempEl.innerHTML = "Temperature: " + data.list[0].main.temp + " F";
+                    currTempEl.classList = "card";
+                    currentEl.appendChild(currTempEl)
+
+                    var currWindEl = document.createElement("div");
+                    currWindEl.innerHTML = "Wind: " + data.list[0].wind.speed + " mph";
+                    currWindEl.classList = "card";
+                    currentEl.appendChild(currWindEl)
+
+                    var currHumEl = document.createElement("div");
+                    currHumEl.innerHTML = "Humidity: " + data.list[0].main.humidity + " %";
+                    currHumEl.classList = "card";
+                    currentEl.appendChild(currHumEl)
+
+                    //Day 1 Card
+                    var day1Title=document.createElement('h4')
+                    day1Title.innerHTML = day1;
+                    day1Title.classList = 'card-header';
+                    document.querySelector('.day1').appendChild(day1Title);
+
+                    var day1El = document.createElement('div');
+                    day1El.innerHTML = "Temperature: " + data.list[1].main.temp + " F <br> Wind: " + data.list[1].wind.speed + " mph <br> Humidity: " + data.list[1].main.humidity + " %";
+                    day1El.classList = "card"
+                    document.querySelector('.day1').appendChild(day1El)
+
+                    //Day 2 Card
+                    var day2Title=document.createElement('h4')
+                    day2Title.innerHTML = day2;
+                    day2Title.classList = 'card-header';
+                    document.querySelector('.day2').appendChild(day2Title);
+
+                    var day2El = document.createElement('div');
+                    day2El.innerHTML = "Temperature: " + data.list[2].main.temp + " F <br> Wind: " + data.list[2].wind.speed + " mph <br> Humidity: " + data.list[2].main.humidity + " %";
+                    day2El.classList = "card"
+                    document.querySelector('.day2').appendChild(day2El)
+
+                     //Day 3 Card
+                     var day3Title=document.createElement('h4')
+                     day3Title.innerHTML = day3;
+                     day3Title.classList = 'card-header';
+                     document.querySelector('.day3').appendChild(day3Title);
+ 
+                     var day3El = document.createElement('div');
+                     day3El.innerHTML = "Temperature: " + data.list[3].main.temp + " F <br> Wind: " + data.list[3].wind.speed + " mph <br> Humidity: " + data.list[3].main.humidity + " %";
+                     day3El.classList = "card"
+                     document.querySelector('.day3').appendChild(day3El)
+
+                      //Day 4 Card
+                    var day4Title=document.createElement('h4')
+                    day4Title.innerHTML = day4;
+                    day4Title.classList = 'card-header';
+                    document.querySelector('.day4').appendChild(day4Title);
+
+                    var day4El = document.createElement('div');
+                    day4El.innerHTML = "Temperature: " + data.list[4].main.temp + " F <br> Wind: " + data.list[4].wind.speed + " mph <br> Humidity: " + data.list[4].main.humidity + " %";
+                    day4El.classList = "card"
+                    document.querySelector('.day4').appendChild(day4El)
+
+
+                     //Day 5 Card
+                     var day5Title=document.createElement('h4')
+                     day5Title.innerHTML = day5;
+                     day5Title.classList = 'card-header';
+                     document.querySelector('.day5').appendChild(day5Title);
+ 
+                     var day5El = document.createElement('div');
+                     day5El.innerHTML = "Temperature: " + data.list[5].main.temp + " F <br> Wind: " + data.list[5].wind.speed + " mph <br> Humidity: " + data.list[5].main.humidity + " %";
+                     day5El.classList = "card"
+                     document.querySelector('.day5').appendChild(day5El)
+ 
+ 
+
 
                 })
         })
