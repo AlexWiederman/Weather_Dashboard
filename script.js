30ef77ac7b088be8f01588c0cfe989946524b4d0
Alex W <amwiederman15@gmail.com>
2022-12-16 12:40:48 -0500

    finished framework for html and css

---
+++ script.js
@@ -0,0 +1,7 @@
+var card5day1El = document.querySelector(".day1-header")
+var card5day2El = document.querySelector(".day2-header")
+var card5day3El = document.querySelector(".day3-header")
+var card5day4El = document.querySelector(".day4-header")
+var card5day5El = document.querySelector(".day5-header")
+
+var currentDay = dayjs().format();
