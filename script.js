30ac27d87b31536f28113ab1bf7757703a875493
Alex W <amwiederman15@gmail.com>
2022-12-17 09:14:49 -0500

    got cards working with api data

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var currentEl = document.querySelector(".current")
 var key = "5ac5953f4965de6704e28a9ae8fcf1ff"
 var lat
 var lon
-var city = "Goshen";
+var city = "Tampa";
 var currentWind;
 var currentTemp;
 var currentHum;
@@ -54,14 +54,14 @@ function getCords() {
             
             // console.log(data[0])
 
-            var weatherInfoUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + key
+            var weatherInfoUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + key
             fetch(weatherInfoUrl)
                 .then(function (response) {
                     return response.json();
                 })
                 .then(function (data) {
-                    console.log(data.list[0])
-                    currentWind = data.list[0]
+                    console.log(data.list)
+                    
 
                     // Current Day Card
                     var currTitleEl = document.createElement('h4');
@@ -69,26 +69,26 @@ function getCords() {
                     currTitleEl.classList = "card-header"
                     currentEl.appendChild(currTitleEl)
 
+                    var currImgEl = document.createElement("img");
+                    currImgEl.src = "http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png"
+                    currImgEl.classList = "card"
+                    currentEl.appendChild(currImgEl)
+
                     var currTempEl = document.createElement("div");
-                    currTempEl.innerHTML = "Temperature: " + data.list[0].main.temp + " F";
+                    currTempEl.innerHTML = "Temperature: " + data.list[0].main.temp + " F<br> Wind: " + data.list[0].wind.speed + " mph<br> Humidity: " + data.list[0].main.humidity + " %";
                     currTempEl.classList = "card";
                     currentEl.appendChild(currTempEl)
-
-                    var currWindEl = document.createElement("div");
-                    currWindEl.innerHTML = "Wind: " + data.list[0].wind.speed + " mph";
-                    currWindEl.classList = "card";
-                    currentEl.appendChild(currWindEl)
-
-                    var currHumEl = document.createElement("div");
-                    currHumEl.innerHTML = "Humidity: " + data.list[0].main.humidity + " %";
-                    currHumEl.classList = "card";
-                    currentEl.appendChild(currHumEl)
 
                     //Day 1 Card
                     var day1Title=document.createElement('h4')
                     day1Title.innerHTML = day1;
                     day1Title.classList = 'card-header';
                     document.querySelector('.day1').appendChild(day1Title);
+
+                    var day1ImgEl = document.createElement("img");
+                    day1ImgEl.src = "http://openweathermap.org/img/wn/" + data.list[1].weather[0].icon + "@2x.png"
+                    day1ImgEl.classList = "card"
+                    document.querySelector('.day1').appendChild(day1ImgEl)
 
                     var day1El = document.createElement('div');
                     day1El.innerHTML = "Temperature: " + data.list[1].main.temp + " F <br> Wind: " + data.list[1].wind.speed + " mph <br> Humidity: " + data.list[1].main.humidity + " %";
@@ -101,6 +101,11 @@ function getCords() {
                     day2Title.classList = 'card-header';
                     document.querySelector('.day2').appendChild(day2Title);
 
+                    var day2ImgEl = document.createElement("img");
+                    day2ImgEl.src = "http://openweathermap.org/img/wn/" + data.list[2].weather[0].icon + "@2x.png"
+                    day2ImgEl.classList = "card"
+                    document.querySelector('.day2').appendChild(day2ImgEl)
+
                     var day2El = document.createElement('div');
                     day2El.innerHTML = "Temperature: " + data.list[2].main.temp + " F <br> Wind: " + data.list[2].wind.speed + " mph <br> Humidity: " + data.list[2].main.humidity + " %";
                     day2El.classList = "card"
@@ -111,6 +116,11 @@ function getCords() {
                      day3Title.innerHTML = day3;
                      day3Title.classList = 'card-header';
                      document.querySelector('.day3').appendChild(day3Title);
+
+                     var day3ImgEl = document.createElement("img");
+                    day3ImgEl.src = "http://openweathermap.org/img/wn/" + data.list[3].weather[0].icon + "@2x.png"
+                    day3ImgEl.classList = "card"
+                    document.querySelector('.day3').appendChild(day3ImgEl)
  
                      var day3El = document.createElement('div');
                      day3El.innerHTML = "Temperature: " + data.list[3].main.temp + " F <br> Wind: " + data.list[3].wind.speed + " mph <br> Humidity: " + data.list[3].main.humidity + " %";
@@ -123,6 +133,11 @@ function getCords() {
                     day4Title.classList = 'card-header';
                     document.querySelector('.day4').appendChild(day4Title);
 
+                    var day4ImgEl = document.createElement("img");
+                    day4ImgEl.src = "http://openweathermap.org/img/wn/" + data.list[4].weather[0].icon + "@2x.png"
+                    day4ImgEl.classList = "card"
+                    document.querySelector('.day4').appendChild(day4ImgEl)
+
                     var day4El = document.createElement('div');
                     day4El.innerHTML = "Temperature: " + data.list[4].main.temp + " F <br> Wind: " + data.list[4].wind.speed + " mph <br> Humidity: " + data.list[4].main.humidity + " %";
                     day4El.classList = "card"
@@ -134,6 +149,11 @@ function getCords() {
                      day5Title.innerHTML = day5;
                      day5Title.classList = 'card-header';
                      document.querySelector('.day5').appendChild(day5Title);
+
+                     var day5ImgEl = document.createElement("img");
+                    day5ImgEl.src = "http://openweathermap.org/img/wn/" + data.list[5].weather[0].icon + "@2x.png"
+                    day5ImgEl.classList = "card"
+                    document.querySelector('.day5').appendChild(day5ImgEl)
  
                      var day5El = document.createElement('div');
                      day5El.innerHTML = "Temperature: " + data.list[5].main.temp + " F <br> Wind: " + data.list[5].wind.speed + " mph <br> Humidity: " + data.list[5].main.humidity + " %";
